00f2c9178d6b9f3ec1706bf2e124cee6dc28125d
Lili Clift <liliclift@gmail.com>
2023-09-03 23:40:23 -0700

    intro files

---
+++ evco/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: white !important;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img alt="Evco Logo" class="logo" src="./assets/evcoLogo.png" width="100" height="80" />
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <RouterLink class="nav-link active" to="/">Home</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/about">About Us</RouterLink>
+          </li>
+        </ul>
+        <form class="d-flex col-6" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="./assets/evcoLogo.png" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+
+  <RouterView />
+</template>
