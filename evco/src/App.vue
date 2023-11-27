ef8d33720cd097d25a2dfa3be2c3c6f8e96faff7
Lili Clift <liliclift@gmail.com>
2023-11-26 22:56:06 -0800

    local host reference

--- evco/src/App.vue
+++ evco/src/App.vue
@@ -103,7 +103,7 @@ const sliderValue = ref(0);
         state.productName = name;
       },
       GetCategories() {
-        fetch('http://localhost:1433/api/data/categories/parents')
+        fetch('https://mango-pebble-0e2297810.4.azurestaticapps.net/api/data/categories/parents')
           .then((response) => {
             if (!response.ok) {
               throw new Error('Network response was not ok');
