fe61d099099a6b916915815ce530628279cae510
Lili Clift <liliclift@gmail.com>
2023-11-27 21:26:55 -0800

    configuring vite

--- evco/vite.config.js
+++ evco/vite.config.js
@@ -1,13 +1,27 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import copy from 'rollup-plugin-copy';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+      rollupOptions: {
+          plugins: [
+              copy({
+                  targets: [
+                      { src: 'server.js', dest: 'dist' },
+                      { src: 'api.js', dest: 'dist' },
+                  ],
+                  hook: 'writeBundle'
+              }),
+          ]
+      }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
