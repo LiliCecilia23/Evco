ac5e4402d1930234ebe43ef5d211646f9a6bd403
Lili Clift <liliclift@gmail.com>
2024-03-06 13:36:34 -0800

    deleted evco files, new favicon

--- src/components/Subcategory.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-defineProps(['item']);
-</script>
-<template>
-    <div class="container">
-        <img :src="item.imgUrl" style="height: 50px;">
-        <p style="text-align: center !important;">{{ item.name }}</p>
-    </div>
-</template>
